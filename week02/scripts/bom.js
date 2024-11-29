35243f6e7db1596b248d3c7b65dd90a995e2d57d
alynwhite <alexia.white135@gmail.com>
2024-11-28 17:42:00 -0700

    wrote bom code

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,33 @@ const input = document.querySelector('#favChap');
 const button = document.querySelector('button');
 const listScriptures = document.querySelector('#list');
 
-let favChapEntriesList = document.createElement("li");
-let deleteButton = document.createElement('button');
+// let favChapEntriesList = document.createElement("li");
+// let deleteButton = document.createElement('button');
 
-favChapEntriesList.textContent = input.value;
-deleteButton.textContent = '❌';
+// favChapEntriesList.textContent = input.value;
+// deleteButton.textContent = '❌';
 
-favChapEntriesList.append(deleteButton);
-listScriptures.append(favChapEntriesList);
+// favChapEntriesList.append(deleteButton);
+// listScriptures.append(favChapEntriesList);
+
+button.addEventListener('click', function(){
+    if(input.value.trim() !== ''){
+        let favChapEntriesList = document.createElement("li");
+        let deleteButton = document.createElement('button');
+
+        favChapEntriesList.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        favChapEntriesList.append(deleteButton);
+        listScriptures.append(favChapEntriesList);
+        deleteButton.addEventListener('click', function(){
+            listScriptures.removeChild(li);
+            input.focus();
+        });
+    }
+    else{
+        alert('Nothing in chapter');
+    }
+    input.value = '';
+    input.focus();
+});
