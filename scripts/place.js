1b11cb1a2ea58a787e1815260f030657f4add933
alynwhite <alexia.white135@gmail.com>
2024-12-08 00:20:12 -0700

    fixed all of javascript

--- scripts/place.js
+++ scripts/place.js
@@ -5,5 +5,21 @@ const today = new Date();
 currentyear.innerHTML = `<span>${today.getFullYear()}  | Alexia White | Utah, USA</span>  `;
 lastModified.innerHTML = `Last Modified: ${document.lastModified}`; 
 
-const wind = 20;
-const temperature = 29;
+const windchill = document.querySelector('#windchill');
+
+function windChillFactor(){
+    let wind = 20;
+    let temperature = 29;
+    if((wind > 3) && (temperature <=50)){
+        calculateWindChill(wind, temperature);
+    }
+    else{
+     windchill.innerHTML = `N/A`;
+    }
+}
+function calculateWindChill(wind, temperature){
+    const windChillCalc = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temperature * Math.pow(wind, 0.16));
+    
+    windchill.innerHTML = windChillCalc.toFixed(2);
+}
+windChillFactor();
